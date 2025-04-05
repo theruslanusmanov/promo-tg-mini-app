95128af2e781128c98a3a32bd66547d22c199ece
Ruslan Usmanov <Ruslan.Usmanov@mvideo.ru>
2025-04-05 05:16:37 +0300

    add example

--- promo-api/src/main.rs
+++ promo-api/src/main.rs
@@ -1,57 +1,129 @@
+//! Run with
+//!
+//! ```not_rust
+//! cargo run -p promo-api
+//! ```
+//!
+//! Checkout the [diesel webpage](https://diesel.rs) for
+//! longer guides about diesel
+//!
+//! Checkout the [crates.io source code](https://github.com/rust-lang/crates.io/)
+//! for a real world application using axum and diesel
+
 use axum::{
-    routing::{get, post},
+    extract::State,
     http::StatusCode,
-    Json, Router,
+    response::Json,
+    routing::{get, post},
+    Router,
 };
-use serde::{Deserialize, Serialize};
+use diesel::prelude::*;
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+use std::net::SocketAddr;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+// this embeds the migrations into the application binary
+// the migration path is relative to the `CARGO_MANIFEST_DIR`
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations/");
+
+// normally part of your generated schema.rs file
+table! {
+    users (id) {
+        id -> Integer,
+        name -> Text,
+        hair_color -> Nullable<Text>,
+    }
+}
+
+#[derive(serde::Serialize, Selectable, Queryable)]
+struct User {
+    id: i32,
+    name: String,
+    hair_color: Option<String>,
+}
+
+#[derive(serde::Deserialize, Insertable)]
+#[diesel(table_name = users)]
+struct NewUser {
+    name: String,
+    hair_color: Option<String>,
+}
 
 #[tokio::main]
 async fn main() {
-    // initialize tracing
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| format!("{}=debug", env!("CARGO_CRATE_NAME")).into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-    // build our application with a route
+    let db_url = std::env::var("DATABASE_URL").unwrap();
+
+    // set up connection pool
+    let manager = deadpool_diesel::postgres::Manager::new(db_url, deadpool_diesel::Runtime::Tokio1);
+    let pool = deadpool_diesel::postgres::Pool::builder(manager)
+        .build()
+        .unwrap();
+
+    // run the migrations on server startup
+    {
+        let conn = pool.get().await.unwrap();
+        conn.interact(|conn| conn.run_pending_migrations(MIGRATIONS).map(|_| ()))
+            .await
+            .unwrap()
+            .unwrap();
+    }
+
+    // build our application with some routes
     let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(root))
-        // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+        .route("/user/list", get(list_users))
+        .route("/user/create", post(create_user))
+        .with_state(pool);
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    // run it with hyper
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {addr}");
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-// basic handler that responds with a static string
-async fn root() -> &'static str {
-    "Hello, World!"
-}
-
 async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
+    State(pool): State<deadpool_diesel::postgres::Pool>,
+    Json(new_user): Json<NewUser>,
+) -> Result<Json<User>, (StatusCode, String)> {
+    let conn = pool.get().await.map_err(internal_error)?;
+    let res = conn
+        .interact(|conn| {
+            diesel::insert_into(users::table)
+                .values(new_user)
+                .returning(User::as_returning())
+                .get_result(conn)
+        })
+        .await
+        .map_err(internal_error)?
+        .map_err(internal_error)?;
+    Ok(Json(res))
 }
 
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
+async fn list_users(
+    State(pool): State<deadpool_diesel::postgres::Pool>,
+) -> Result<Json<Vec<User>>, (StatusCode, String)> {
+    let conn = pool.get().await.map_err(internal_error)?;
+    let res = conn
+        .interact(|conn| users::table.select(User::as_select()).load(conn))
+        .await
+        .map_err(internal_error)?
+        .map_err(internal_error)?;
+    Ok(Json(res))
 }
 
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
+/// Utility function for mapping any error into a `500 Internal Server Error`
+/// response.
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
