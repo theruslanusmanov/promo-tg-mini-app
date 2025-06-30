5c5293c12a19c53384f686d8cf1509996d3976a8
Ruslan Usmanov <ruslanusm6@gmail.com>
2025-06-30 10:21:46 +0300

    add ng

---
+++ promo-app/src/app/app.ts
@@ -0,0 +1,23 @@
+import {Component, signal} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  list = signal([
+    'Заявка 1',
+    'Заявка 2',
+    'Заявка 3',
+  ])
+
+  add() {
+    console.log('ADD')
+    const list = this.list();
+    list.push('Заявка ' + (this.list().length + 1))
+    this.list.set(list);
+    console.log(this.list());
+  }
+}
