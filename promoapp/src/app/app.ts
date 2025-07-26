a60f2d9b43557466cdbf2454f038b643d958bfaf
Ruslan Usmanov <ruslanusm6@gmail.com>
2025-07-26 18:02:58 +0300

    add ng-zorro

--- promoapp/src/app/app.ts
+++ promoapp/src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NzLayoutModule, NzMenuModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('promoapp');
-}
+export class App {}
