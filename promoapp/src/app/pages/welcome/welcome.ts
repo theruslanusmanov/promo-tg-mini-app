67015cb1dcb9963b8174a187c74cb90c9f5bd349
Ruslan Usmanov <ruslanusm6@gmail.com>
2025-08-03 17:07:30 +0300

    add card

--- promoapp/src/app/pages/welcome/welcome.ts
+++ promoapp/src/app/pages/welcome/welcome.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NzRadioComponent, NzRadioGroupComponent} from 'ng-zorro-antd/radio';
+import {FormsModule} from '@angular/forms';
+import {NzOptionComponent, NzSelectComponent} from 'ng-zorro-antd/select';
+import {NzInputDirective} from 'ng-zorro-antd/input';
+import {NzButtonComponent} from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-welcome',
-  imports: [],
+  imports: [
+    NzRadioGroupComponent,
+    NzRadioComponent,
+    FormsModule,
+    NzSelectComponent,
+    NzOptionComponent,
+    NzInputDirective,
+    NzButtonComponent
+  ],
   templateUrl: './welcome.html',
   styleUrl: './welcome.scss'
 })
-export class Welcome {}
+export class Welcome {
+  value = ''
+  radioValue = 'A';
+  radioValue2 = 'Apple';
+  options = [
+    { label: 'Пост', value: 'Apple' },
+    { label: 'Сторис', value: 'Pear' },
+    { label: 'Рассылка', value: 'Orange' }
+  ];
+}
